f0cd316d8df2400edf3e753a114f430a35632663
sshyta <sshyta@mail.ru>
2025-04-20 14:33:57 +0300

    Update: telescope/telescope.nvim

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 
 vim.api.nvim_set_keymap('n', '<Leader>e', ':Neotree toggle<CR>', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope find files' })
